6c0d119b16267627dc8d84ecdf7b65f8941f5cf3
Shawn Michaud <shawnmne@hotmail.com>
2016-04-04 15:21:37 -0500

    begin project

--- main.rb
+++ main.rb
@@ -2,5 +2,8 @@ require_relative "config/environment.rb"
 require "active_record"
 require "pry"
 require "yaml"
-
+require_relative "lib/member.rb"
+require_relative "lib/topic.rb"
+require_relative "lib/meetup.rb"
+require_relative "lib/membership_type.rb"
 binding.pry
